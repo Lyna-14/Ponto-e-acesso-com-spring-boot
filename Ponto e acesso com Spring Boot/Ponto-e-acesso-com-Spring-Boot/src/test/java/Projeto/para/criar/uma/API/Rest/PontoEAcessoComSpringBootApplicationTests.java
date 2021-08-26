91dc2abb7a1d52f223179a34712a05162a6b682c
Lyna-14 <89401594+Lyna-14@users.noreply.github.com>
2021-08-25 21:05:24 -0300

    Add files via upload

---
+++ Ponto e acesso com Spring Boot/Ponto-e-acesso-com-Spring-Boot/src/test/java/Projeto/para/criar/uma/API/Rest/PontoEAcessoComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.para.criar.uma.API.Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoEAcessoComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
